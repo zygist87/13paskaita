3be98659f5d95936f0c6e74da268697eb85420a7
zygist87 <zygist87@gmail.com>
2018-11-21 19:47:42 +0200

    klaseje pirmas

---
+++ assets/app.js
@@ -0,0 +1,60 @@
+function printallNames (name1, name2, name3) {
+  return `Here are all the names: ${name1}, ${name2}, ${name3}`;
+}
+console.log(printallNames(`Tomas`, `Martynas`, `Robertas`))
+
+console.log("------------")
+
+var skaicius = (prompt("skaicius"));
+function counter (skaicius) {
+  for (var i = 1; i<= skaicius; i++) {
+    console.log(i);
+  }
+}
+counter(skaicius);
+console.log("-----------")
+
+function sveikiname(skaicius) {
+  var text = "";
+  for (var i = 1; i <= skaicius; i++) {
+    if (i === skaicius) {
+      text = text + "valio!";
+    } else {
+      text = text + "valio,"
+    }
+  }
+  return text;
+}
+console.log(sveikiname(3));
+
+console.log("------------")
+
+function sum(x, y) {
+  return x + y;
+}
+function dividedByTwo(num) {
+  return sum / 2;
+}
+let result = sum(10, 8);
+dividedByTwo(result);
+console.log(result);
+console.log("-------------");
+
+function makeLine(length) {
+  var line = "";
+  for (var j = 1; j <= length; j++) {
+    line += "*"
+  }
+  return line + "\n";
+}
+function buildTriangle(num) {
+  let result = "";
+  for (let i = 1; i <= num; i++) {
+    result = result + makeLine(i);
+  }
+  return result;
+
+}
+console.log(buildTriangle(20));
+
+console.log("------------------------------");
