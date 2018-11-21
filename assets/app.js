0260e0c9a8e097d5132d2e56cae5d6414bf8dd3c
zygist87 <zygist87@gmail.com>
2018-11-21 21:51:14 +0200

    paskutinis saugojimas foreach ir panasus dalykai

--- assets/app.js
+++ assets/app.js
@@ -63,3 +63,90 @@ let sayHello = function() {
   return "hello my friend";
 }
 console.log(sayHello());
+console.log("------------------");
+
+let delivery = true;
+let destination = "Ukmerge";
+let priveItem = 40;
+let costDelivery = 20;
+let productArr = [true, "Ukmerge", 40, 20];
+console.log(productArr);
+console.log(productArr.length)
+
+console.log("------------------");
+
+let adelivery = true;
+let adestination = "Ukmerge";
+let apriveItem = 40;
+let acostDelivery = 20;
+let aproductArr = [true, "Ukmerge", 40, 20];
+console.log(productArr.push("Zarasai"));
+console.log(productArr);
+console.log(productArr[productArr.length - 1]);
+
+console.log("------------------");
+
+let aadelivery = true;
+let aadestination = "Ukmerge";
+let aapriveItem = 40;
+let aacostDelivery = 20;
+let aaproductArr = [true, "Ukmerge", 40, 20];
+console.log(aaproductArr.pop());
+console.log(aaproductArr);
+
+console.log("-------------");
+let aaaproductArr = [true, "Ukmerge", 40, 20];
+console.log(aaaproductArr.splice(2, 0, "naujas elementas"));
+console.log(aaaproductArr);
+
+console.log("------------------------");
+var rainbow = ["red", "orange", "blacberry", "blue"];
+rainbow.splice(2, 2, "yellow", "gree", "blue", "purple");
+console.log(rainbow);
+
+console.log("-------------------");
+
+let team = ["player1", "player2", "player3", "player4", "player5", "player6", "player7"];
+function hasEnoughPlayers() {
+  if (team.length >= 7) {
+    return ("team has 7, it is enough");
+  } else {
+    return ("team hasn't got enough players");
+  }
+}
+console.log(team);
+console.log("-------------------");
+
+let aaaaproductArr = [true, "Ukmerge", 40, "blablabla", 20];
+rainbow.splice(2, 2, "yellow", "gree", "blue", "purple");
+console.log(aaaaproductArr);
+console.log(aaaaproductArr.join("*"));
+console.log(aaaaproductArr.includes(20));
+console.log("-----------------");
+
+let blabla = ["vilnius", "kaunas", "siauliai", "marijampole"];
+function didziosios(param) {
+  for (let i = 0; i < param.length; i++) {
+    param[i] = param[i].toUpperCase();
+  }
+  return param;
+}
+console.log(didziosios(blabla));
+
+console.log("-----------------");
+
+var stai = ["vilnius", "kaunas", "siauliai", "marijampole"];
+stai.forEach(function(element) {
+  console.log(element);
+});
+console.log("----------------------");
+
+let city = ["vilnius", "kaunas", "siauliai", "marijampole"];
+function myArrFunc(param) {
+  let arr2 = []
+  param.forEach(function(item) {
+    arr2.push(item.toUpperCase());
+  });
+  return arr2;
+}
+  console.log(myArrFunc(city));
